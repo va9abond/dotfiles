35818504f3eecdb4484966a1e4ebb61937d5ac97
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2023-12-10 09:03:37 +0300

    chore: start ignoring nvim config

--- .config/nvim/after/plugin/barbecue.lua
+++
@@ -1,9 +0,0 @@
--- ---------------------------------------------------------
-                 -- https://github.com/utilyre/barbecue.nvim
--- ---------------------------------------------------------
-require('barbecue').setup({
-    show_dirname = true,
-    show_basename = false,
-    exclude_filetypes = { "netrw", "toggleterm", "tex", "pdf", "txt", "text",
-                            "wiki", "Trouble" },
-})
